c5717645ad9d70fa91cff1ca99838cb63f05c1af
Đặng Thanh Truyền <thanhtruyen1504@gmail.com>
2020-11-13 19:56:32 +0700

    Update theme

---
+++ wordpress/wp-content/themes/customtheme/footer.php
@@ -0,0 +1,53 @@
+<?php
+   /**
+    * The template for displaying the footer
+    *
+    * Contains the closing of the #content div and all content after.
+    *
+    * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+    *
+    * @package thietbitheduc
+    */
+   
+   ?>
+<footer id="colophon" class="site-footer">
+   <div class="site-info">
+      <div class="container">
+         <div id="footer">
+            <div class="container">
+               <div class="row">
+                  <div class="col-md-4">
+                     <div class="info">
+                        <div class="title_footer">KHOA CÔNG NGHỆ THÔNG TIN <br> Trường Cao Đẳng Công Nghệ Thủ Đức</div>
+                        <div class="add">53 Võ Văn Ngân, P.Linh Chiểu, Q.Thủ Đức, Tp.HCM</div>
+                        <div class="fax">Fax: (028)38962474</div>
+                        <div class="email">Email: fit@tdc.edu.vn</div>
+                        <div class="fb">Facebook: facebook.com/tdc.fit</div>
+                        <div class="yt">Youtube: youtube.com/fit-tdc</div>
+                     </div>
+                  </div>
+                  <div class="col-md-4">
+                     <ul class="list_ul">
+                        <li><a href="gioi-thieu">Giới thiệu</a></li>
+                        <li><a href="http://tdc.edu.vn/">Trường Cao Đẳng Công Nghệ Thủ Đức</a></li>
+                     </ul>
+                  </div>
+                  <div class="col-md-4">
+                     <ul class="list_ul">
+                        <li><a href="lien-he">Liên hệ</a></li>
+                        <li><a href="http://tdc.edu.vn/">2020 Khoa Công Nghệ Thông Tin <span> | </span> Công Nghệ Thủ Đức <span> | </span> FIT-TDC All Rights Reserved.</a></li>
+                     </ul>
+                  </div>
+               </div>
+            </div>
+            <div class="clear"></div>
+         </div>
+      </div>
+   </div>
+   <!-- .site-info -->
+</footer>
+<!-- #colophon -->
+</div><!-- #page -->
+<?php wp_footer(); ?>
+</body>
+</html>
---
+++ wordpress/wp-content/themes/customtheme/single.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * The template for displaying all single posts
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
+ *
+ * @package thietbitheduc
+ */
+
+get_header();
+?>
+
+	<main id="primary" class="site-main container">
+	<div class="row">
+		<div class="col-md-8">
+		<?php
+		while ( have_posts() ) :
+			the_post();
+
+			get_template_part( 'template-parts/content', get_post_type() );
+
+			the_post_navigation(
+				array(
+					'prev_text' => '<span class="nav-subtitle">' . esc_html__( 'Previous:', 'thietbitheduc' ) . '</span> <span class="nav-title">%title</span>',
+					'next_text' => '<span class="nav-subtitle">' . esc_html__( 'Next:', 'thietbitheduc' ) . '</span> <span class="nav-title">%title</span>',
+				)
+			);
+
+			// If comments are open or we have at least one comment, load up the comment template.
+			if ( comments_open() || get_comments_number() ) :
+				comments_template();
+			endif;
+
+		endwhile; // End of the loop.
+		?>
+		</div>
+		
+		<div class="col-md-4 left-home">
+		<?php get_sidebar(); ?>
+		</div>
+	</div>
+		
+
+	</main><!-- #main -->
+
+<?php
+
+get_footer();
